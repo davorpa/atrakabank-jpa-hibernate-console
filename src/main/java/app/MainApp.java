31db4dec1e755271b334e8b42ca44993de4dbeee
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-02-27 19:29:47 +0100

    Init proyecto Java-Maven con Lombok, MySql, JPA e Hibernate

---
+++ src/main/java/app/MainApp.java
@@ -0,0 +1,12 @@
+package app;
+
+/**
+ * Clase para probar el ejercicio de los atracadores.
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
