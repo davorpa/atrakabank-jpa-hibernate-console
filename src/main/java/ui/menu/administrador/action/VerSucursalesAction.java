f4c9a46d7bdeb50a84d831bf1d8ddfa98930db17
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-03-02 10:18:35 +0100

    Implement opcion de menu lista sucursales de banco

--- src/main/java/ui/menu/administrador/action/VerSucursalesAction.java
+++ src/main/java/ui/menu/administrador/action/VerSucursalesAction.java
@@ -1,5 +1,16 @@
 package ui.menu.administrador.action;
 
+import static util.console.Console.printf;
+import static util.console.Console.readString;
+
+import java.util.List;
+
+import business.model.Banco;
+import business.model.Sucursal;
+import business.service.BancoService;
+import business.service.ServiceFactory;
+import business.service.SucursalService;
+import util.console.Console;
 import util.menu.Action;
 
 /**
@@ -9,7 +20,17 @@ public class VerSucursalesAction implements Action {
 
 	@Override
 	public void execute() throws Exception {
-		// TODO Auto-generated method stub
+		BancoService bancoService = ServiceFactory.getInstance().getBancoService();
+
+		List<Banco> bancos = bancoService.findAll();
+		bancos.forEach(Console::println);
+		String codigo = readString("Código del banco");
+
+		SucursalService sucursalService = ServiceFactory.getInstance().getSucursalService();
+
+		List<Sucursal> sucursales = sucursalService.findByCodigoBanco(codigo);
+		sucursales.forEach(Console::println);
+		printf("Número de sucursales: %s%n", sucursales.size());
 	}
 
 }
