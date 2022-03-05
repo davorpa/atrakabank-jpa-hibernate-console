ce282997476dd961f57a58f6103705bbf8e5aa94
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-03-05 23:48:31 +0100

    format output

--- src/main/java/ui/menu/administrador/action/VerSucursalesAction.java
+++ src/main/java/ui/menu/administrador/action/VerSucursalesAction.java
@@ -1,6 +1,7 @@
 package ui.menu.administrador.action;
 
 import static util.console.Console.printf;
+import static util.console.Console.println;
 import static util.console.Console.readString;
 
 import java.util.List;
@@ -23,14 +24,31 @@ public class VerSucursalesAction implements Action {
 		BancoService bancoService = ServiceFactory.getInstance().getBancoService();
 
 		List<Banco> bancos = bancoService.findAll();
+		if (bancos.isEmpty()) {
+			printf("%n%nINFO :: Todavía no hay bancos.%n%n");
+			return;
+		}
+		println("");
+		println("                        ╔════════════════════════════╗                        ");
+		println("  ◄╔════════════════════╝►          BANCOS          ◄╚════════════════════╗►  ");
 		bancos.forEach(Console::println);
-		String codigo = readString("Código del banco");
+		println("  ◄╚══════════════════════════════╣► ♦♦♦♦ ◄╠══════════════════════════════╝►  ");
+
+		String codigo = readString("   Código del banco");
 
 		SucursalService sucursalService = ServiceFactory.getInstance().getSucursalService();
 
 		List<Sucursal> sucursales = sucursalService.findByCodigoBanco(codigo);
+		if (sucursales.isEmpty()) {
+			printf("%n%nINFO :: La búsqueda no ha devuelto resultados.%n%n");
+			return;
+		}
+		println("");
+		println("                        ╔════════════════════════════╗                        ");
+		println("  ◄╔════════════════════╝►        SUCURSALES        ◄╚════════════════════╗►  ");
 		sucursales.forEach(Console::println);
-		printf("Número de sucursales: %s%n", sucursales.size());
+		println("  ◄╚══════════════════════════════╣► ♦♦♦♦ ◄╠══════════════════════════════╝►  ");
+		printf("         Número de sucursales: %3s%n", sucursales.size());
 	}
 
 }
