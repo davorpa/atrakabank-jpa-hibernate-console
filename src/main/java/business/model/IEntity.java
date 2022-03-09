0590ac2ab3e03cd596b338354b700797d71b32d5
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-03-09 15:12:41 +0100

    Add toString, hashcode, equals al contrato Entity

--- src/main/java/business/model/IEntity.java
+++ src/main/java/business/model/IEntity.java
@@ -4,6 +4,21 @@ import java.io.Serializable;
 
 public interface IEntity<PK extends Serializable> extends Serializable {
 
+	/**
+	 * Obtiene el valor de la clave artificial que identifica unequívocamente
+	 * la entidad de persistencia subyacente.
+	 *
+	 * @return nunca {@code}
+	 */
 	PK getId();
+
+	@Override
+	String toString();
+
+	@Override
+	int hashCode();
+
+	@Override
+	boolean equals(Object obj);
 
 }
