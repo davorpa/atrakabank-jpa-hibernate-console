47d5f3b23127e23ad8d7260bb28e55b9ec8e72a9
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-03-10 08:54:48 +0100

    doc: fix javadoc

--- src/main/java/business/model/IEntity.java
+++ src/main/java/business/model/IEntity.java
@@ -8,17 +8,45 @@ public interface IEntity<PK extends Serializable> extends Serializable {
 	 * Obtiene el valor de la clave artificial que identifica unequívocamente
 	 * la entidad de persistencia subyacente.
 	 *
-	 * @return nunca {@code}
+	 * @return nunca {@code null}
 	 */
 	PK getId();
 
+	/**
+	 * Obtiene una representación textual de la entidad de persistencia subyacente.
+	 * <p>
+	 * {@inheritDoc}
+	 * 
+	 * @return
+	 */
 	@Override
 	String toString();
 
+	/**
+	 * Dos instancias que representen una entidad de persistencia son iguales
+	 * si implementan {@link #hashCode()} e {@link #equals(Object)} sobre alguna
+	 * de las claves de la misma, ya sean artificiales o naturales.
+	 * <p>
+	 * {@inheritDoc}
+	 * 
+	 * @return hashcode
+	 * @see #getId()
+	 */
 	@Override
 	int hashCode();
 
+	/**
+	 * Dos instancias que representen una entidad de persistencia son iguales
+	 * si implementan {@link #hashCode()} e {@link #equals(Object)} sobre alguna
+	 * de las claves de la misma, ya sean artificiales o naturales.
+	 * <p>
+	 * {@inheritDoc}
+	 * 
+	 * @param obj la instancia con la que comparar
+	 * @return {@code true} si son equivalentes
+	 * @see #getId()
+	 */
 	@Override
-	boolean equals(Object obj);
+	boolean equals(final Object obj);
 
 }
